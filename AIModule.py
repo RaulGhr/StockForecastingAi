56b5bc995970419becb6470bf4e35c2f9e4e258e
RaulGhr <raul.gherman2804@gmail.com>
2023-08-25 17:33:46 +0300

    hooks

--- AIModule.py
+++ AIModule.py
@@ -1,10 +1,94 @@
-import numpy as np
 import pandas as pd
+import datetime
 from neuralforecast import NeuralForecast
 from neuralforecast.models import LSTM
+import matplotlib.pyplot as plt
 
-pd.set_option('display.max_columns', None)
-# intData = pd.read_csv("dateInterogate.csv")
-# print(intData)
+pd.set_option("display.max_columns", None)
+intData = pd.read_csv("dateInterogate.csv")
+intData["ds"] = pd.to_datetime(intData["ds"], format="%Y-%m-%dT%H:%M:%S.%f+0000")
+print(intData.loc[0, "ds"])
+dt = datetime.datetime(2023, 2, 1)
+firstTrainingSet = intData[intData["ds"] < dt]
+# print(firstTrainingSet)
 horizon = 30
-model = LSTM(h=horizon)
+
+
+def createModel():
+    model = [
+        LSTM(
+            h=horizon,
+            max_steps=100,  # Number of steps to train
+            scaler_type="standard",  # Type of scaler to normalize data
+            encoder_hidden_size=64,  # Defines the size of the hidden state of the LSTM
+            decoder_hidden_size=64,
+            hist_exog_list=[
+                "FedInterest",
+                "High",
+                "Low",
+                "Open",
+                "SMA_1M",
+                "SMA_1W",
+                "SMA_3M",
+                "SMA_V_1M",
+                "SMA_V_1W",
+                "SMA_V_3M",
+                "Volume",
+                "Week Number",
+                "Week Number Average",
+            ],
+        )
+    ]
+    nf = NeuralForecast(models=model, freq="B")
+    nf.fit(df=firstTrainingSet)
+    nf.save(path="./testModel", model_index=None, overwrite=True, save_dataset=True)
+
+
+def trainModel():
+    nf2 = NeuralForecast.load(path="./testModel")
+
+    nf2.fit(df=firstTrainingSet)
+    nf2.save(path="./testModel", model_index=None, overwrite=True, save_dataset=True)
+
+
+def predict():
+    nf = NeuralForecast.load(path="./testModel")
+    start_of_feb = datetime.datetime(2023, 2, 1, 0, 0, 0)
+    firstCVSet = intData[intData["ds"].dt.month == start_of_feb.month]
+    firstCVSet = firstCVSet[firstCVSet["ds"].dt.year == start_of_feb.year]
+    print(firstCVSet)
+    Y_hat_df = nf.predict()
+    print(Y_hat_df)
+
+    # fig, ax = plt.subplots(1, 1, figsize=(20, 7))
+    # plot_df = pd.concat([firstCVSet, Y_hat_df]).set_index('ds')  # Concatenate the train and forecast dataframes
+    # plot_df[['y', 'LSTM']].plot(ax=ax, linewidth=2)
+    #
+    # ax.set_title('Results', fontsize=22)
+    # ax.set_ylabel('Monthly Passengers', fontsize=20)
+    # ax.set_xlabel('Timestamp [t]', fontsize=20)
+    # ax.legend(prop={'size': 15})
+    # ax.grid()
+    # Creează un plot
+    # Afișează datele
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    ax.plot(firstCVSet["ds"], firstCVSet["y"], label="firstCVSet")
+    ax.plot(Y_hat_df["ds"], Y_hat_df["LSTM"], label="Y_hat_df (LSTM)")
+
+    ax.set_title("Comparison of firstCVSet and Y_hat_df (LSTM)", fontsize=16)
+    ax.set_xlabel("Date", fontsize=14)
+    ax.set_ylabel("Values", fontsize=14)
+
+    ax.legend()
+    ax.grid()
+    plt.show()
+
+
+if __name__ == "__main__":
+    # createModel()
+    # for i in range(20):
+    #     print(i)
+    trainModel()
+    predict()
+    exit()
--- colectData.py
+++ colectData.py
@@ -3,10 +3,10 @@ import numpy as np
 import yfinance as yf
 import talib
 import datetime
-import pandas as pd
+
 import polars as pl
 from full_fred.fred import Fred
-import os, time
+
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 from google.cloud import secretmanager
@@ -29,87 +29,89 @@ def colectData():
 
     hist = crsr.history(period="max")
 
-    hist = hist.drop('Dividends', axis=1).drop('Stock Splits', axis=1)
+    hist = hist.drop("Dividends", axis=1).drop("Stock Splits", axis=1)
 
     hist = hist.reset_index()
 
     hist = pl.DataFrame(hist)
-    hist = hist.filter((hist['Open'] > 8) & (hist['Open'] < 100))
-    hist = hist.filter((hist['High'] > 8) & (hist['High'] < 100))
-    hist = hist.filter((hist['Low'] > 8) & (hist['Low'] < 100))
-    hist = hist.filter((hist['Close'] > 8) & (hist['Close'] < 100))
-    hist = hist.with_columns(pl.col('Date').cast(pl.Date))
+    hist = hist.filter((hist["Open"] > 8) & (hist["Open"] < 100))
+    hist = hist.filter((hist["High"] > 8) & (hist["High"] < 100))
+    hist = hist.filter((hist["Low"] > 8) & (hist["Low"] < 100))
+    hist = hist.filter((hist["Close"] > 8) & (hist["Close"] < 100))
+    hist = hist.with_columns(pl.col("Date").cast(pl.Date))
 
     # INDICI SAPTAMANA
-    indici_sapt = hist['Date'].to_numpy()
+    indici_sapt = hist["Date"].to_numpy()
     data_ini = indici_sapt[0] - 2
-    indici_sapt = (indici_sapt - data_ini).astype('timedelta64[D]').astype(np.int64) // 7
-    indici_sapt = pl.DataFrame({'Week Number': indici_sapt})
+    indici_sapt = (indici_sapt - data_ini).astype("timedelta64[D]").astype(
+        np.int64
+    ) // 7
+    indici_sapt = pl.DataFrame({"Week Number": indici_sapt})
     hist = hist.hstack(indici_sapt)
 
     # MEDIE PE SAPTAMANA
-    nr_sapt = hist['Week Number'].max() + 1
+    nr_sapt = hist["Week Number"].max() + 1
     suma = [0] * nr_sapt
     frecv = [0] * nr_sapt
     week_nr_avr = []
 
     for row in hist.iter_rows(named=True):
-        indice = row['Week Number']
+        indice = row["Week Number"]
         frecv[indice] += 1
-        suma[indice] += row['Close']
+        suma[indice] += row["Close"]
 
     for i in range(nr_sapt):
         medie = suma[i] / frecv[i]
         for i in range(frecv[i]):
             week_nr_avr.append(medie)
 
-    week_nr_avr = pl.DataFrame({'Week Number Average': week_nr_avr})
+    week_nr_avr = pl.DataFrame({"Week Number Average": week_nr_avr})
     hist = hist.hstack(week_nr_avr)
 
     # INDICI
-    close_prices = hist['Close']
-    volume = hist['Volume']
+    close_prices = hist["Close"]
+    volume = hist["Volume"]
 
     SMA_1W = talib.SMA(close_prices.to_numpy(), timeperiod=5)
-    SMA_1W = pl.DataFrame({'SMA_1W': SMA_1W})
+    SMA_1W = pl.DataFrame({"SMA_1W": SMA_1W})
     hist = hist.hstack(SMA_1W)
 
     SMA_1M = talib.SMA(close_prices.to_numpy(), timeperiod=20)
-    SMA_1M = pl.DataFrame({'SMA_1M': SMA_1M})
+    SMA_1M = pl.DataFrame({"SMA_1M": SMA_1M})
     hist = hist.hstack(SMA_1M)
 
     SMA_3M = talib.SMA(close_prices.to_numpy(), timeperiod=60)
-    SMA_3M = pl.DataFrame({'SMA_3M': SMA_3M})
+    SMA_3M = pl.DataFrame({"SMA_3M": SMA_3M})
     hist = hist.hstack(SMA_3M)
 
     SMA_V_1W = talib.SMA(volume.cast(pl.Float64).to_numpy(), timeperiod=5)
-    SMA_V_1W = pl.DataFrame({'SMA_V_1W': SMA_V_1W})
+    SMA_V_1W = pl.DataFrame({"SMA_V_1W": SMA_V_1W})
     hist = hist.hstack(SMA_V_1W)
 
     SMA_V_1M = talib.SMA(volume.cast(pl.Float64).to_numpy(), timeperiod=20)
-    SMA_V_1M = pl.DataFrame({'SMA_V_1M': SMA_V_1M})
+    SMA_V_1M = pl.DataFrame({"SMA_V_1M": SMA_V_1M})
     hist = hist.hstack(SMA_V_1M)
 
     SMA_V_3M = talib.SMA(volume.cast(pl.Float64).to_numpy(), timeperiod=60)
-    SMA_V_3M = pl.DataFrame({'SMA_V_3M': SMA_V_3M})
+    SMA_V_3M = pl.DataFrame({"SMA_V_3M": SMA_V_3M})
     hist = hist.hstack(SMA_V_3M)
 
     # FED RESERVE DATE
-    fred = Fred('apiKey.txt')
-    dff = fred.get_series_df('DFF')
-    dff = dff.drop('realtime_start', axis=1).drop('realtime_end', axis=1)
+    fred = Fred("apiKey.txt")
+    dff = fred.get_series_df("DFF")
+    dff = dff.drop("realtime_start", axis=1).drop("realtime_end", axis=1)
     dff = pl.DataFrame(dff)
-    dff = dff.with_columns(pl.col('date').str.strptime(pl.Date, format='%Y-%m-%d'))
-    dff = dff.with_columns(pl.col('value').cast(pl.Float64))
+    dff = dff.with_columns(pl.col("date").str.strptime(pl.Date, format="%Y-%m-%d"))
+    dff = dff.with_columns(pl.col("value").cast(pl.Float64))
     dff = dff.rename({"date": "Date", "value": "FedInterest"})
-    hist = hist.join(dff, on='Date', how='left')
+    hist = hist.join(dff, on="Date", how="left")
 
     # GOLIRE CAMPURI GOALE
     hist = hist.fill_nan(None).drop_nulls()
     print("!!DATE COLECTATE!!")
 
-    hist.write_csv('datePrelucrate.csv')
-    hist = hist.with_columns(pl.col('Date').cast(pl.Datetime))
+    hist.write_csv("datePrelucrate.csv")
+    hist = hist.with_columns(pl.col("Date").cast(pl.Datetime))
     return hist
 
 
@@ -134,18 +136,20 @@ def queryData():
 
     result = pl.DataFrame(result)
 
-    result = result.drop(['result', 'table', '_start', '_stop', '_measurement'])
+    result = result.drop(["result", "table", "_start", "_stop"])
 
-    result = result.rename({"_time": "Date"})
+    result = result.rename({"_time": "ds", "_measurement": "unique_id", "Close": "y"})
 
     print(result)
-    result.write_csv('dateInterogate.csv')
+    result.write_csv("dateInterogate.csv")
+
 
 def deleteAllData():
     delete_api = client.delete_api()
     start = datetime.datetime.min
     stop = datetime.datetime.now()
-    delete_api.delete(start, stop, f'_measurement="stock"', bucket=database, org=org)
+    delete_api.delete(start, stop, '_measurement="stock"', bucket=database, org=org)
+
 
 if __name__ == "__main__":
     queryData()
